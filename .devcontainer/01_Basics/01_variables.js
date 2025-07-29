6e026320a125ce97e628e50274eb9f99a5e614a8
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-29 05:27:30 +0000

    commit

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+// const accountID = "144465"
+// let accountEmail = "thorviraut46@gmail.com"
+// const accountCity = "Pune"
+
+// // const accountId = "23"
+
+// console.log(accountID);
+
+
+console.log("Akshata");
