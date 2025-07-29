595f2814929f943e3fdcf73c42223c54f80e4897
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-29 13:50:51 +0000

    commit

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"Use Strict" // treat all js code as newer version
+
+let name = "John"; // string
+let age = 30; // number
+let isStudent = true; // boolean true or false
+let height = null; // null is a standalone value 
+let weight; // undefined
+let hobbies = ["reading", "gaming", "hiking"]; // array
+
+//bigint
+
+//symbol => unique
+
+console.log(typeof "Akshata");
