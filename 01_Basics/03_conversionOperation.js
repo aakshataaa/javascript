30bba4ff5bd2840cea39ea58aa40993874e8491f
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-30 06:48:33 +0000

    operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,24 +1,65 @@
-let score = 33
+// let score = 33
 
-//console.log(typeof score)
-//console.log(typeof(score))
+// //console.log(typeof score)
+// //console.log(typeof(score))
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber)
-//console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// //console.log(typeof valueInNumber)
+// //console.log(valueInNumber)
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn) // true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn) // true
 
-//1 = true, 0 = false
-// "" = false
-//"akshata" = true
+// //1 = true, 0 = false
+// // "" = false
+// //"akshata" = true
 
-let someNumber = 10
+// let someNumber = 10
 
-let stringNumber = String(someNumber)
-console.log(stringNumber) // "10"
-console.log(typeof stringNumber) // "string";
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // "10"
+// console.log(typeof stringNumber) // "string";
+
+
+//--------------------------------operations-----------------------------------
+
+// let str1 = "hello"
+
+// let str2 = " AK"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); 
+// console.log(1 + "2"); 
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+// console.log((3*4) * 5 % 9);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1= num2=num3= 80 % 5
+console.log(num1, num2, num3); 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter); // 101
+
+/******************Increment******************/
+let x=50n
+const y= x++
+console.log(x);
+
+let x2 = 5
+const y2 = ++x2
+console.log(x2); 
+
+
+
+
 
