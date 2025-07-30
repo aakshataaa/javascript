51e14b007598f3ae7611324a62347c4c28555c43
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-30 17:25:25 +0000

    nums and maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,17 @@
+const Score = 400
+console.log(Score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4)); // "123.9"
+
+
+
+
+
+
