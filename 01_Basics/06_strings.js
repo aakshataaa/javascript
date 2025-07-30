05d747dfd43dfd61c0f72631795c343b10d2594f
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-30 16:37:47 +0000

    comparisions

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,19 @@
+const name = "akshata"
+const repoCount = 5
+
+//console.log(name+ repoCount + "Value"); 
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('ak');
+console.log(gameName [0]); // String { "ak" }
+
+console.log(gameName.__proto__); // String.prototype
+console.log(gameName.length); // 2
+console.log(gameName.toUpperCase()); // "AK"
+console.log(gameName.charAt('1'));
+console.log(gameName.indexOf('k')); // 1
+const newString = gameName.substring(0, 1) 
+console.log(newString); // "a"
+
+
