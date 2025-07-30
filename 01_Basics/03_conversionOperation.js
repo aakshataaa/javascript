c8371d7a4bb60acd896980eb4f1225859c9d076c
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-30 06:09:45 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn) // true
+
+//1 = true, 0 = false
+// "" = false
+//"akshata" = true
+
+let someNumber = 10
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) // "10"
+console.log(typeof stringNumber) // "string";
+
