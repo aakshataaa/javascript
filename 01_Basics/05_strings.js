678f3bb953b9c00eebe5fbff550ca77255e32993
aakshataaa <168170637+aakshataaa@users.noreply.github.com>
2025-07-30 16:52:53 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,26 @@
+const name = "akshata"
+const repoCount = 5
+
+//console.log(name+ repoCount + "Value"); 
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('akshata-Raut');
+// console.log(gameName [0]); // String { "ak" }
+
+// console.log(gameName.__proto__); // String.prototype
+// console.log(gameName.length); // 2
+// console.log(gameName.toUpperCase()); // "AK"
+// console.log(gameName.charAt('1'));
+// console.log(gameName.indexOf('k')); // 1
+// const newString = gameName.substring(0, 1) 
+// console.log(newString); // "a"
+
+
+const url = "https://akshata.com/akshata%20raut"
+console.log(url.replace('%20','-'))
+console.log(url.includes('AKSHU'))
+
+console.log(gameName.split('-'));
+
+
